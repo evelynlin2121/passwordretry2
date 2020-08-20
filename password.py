2f8565d8330063ffea5b49eb8c286c5caac22472
Evelyn Lin <xx800521@gmail.com>
2020-08-21 02:39:49 +0800

    add chance

--- password.py
+++ password.py
@@ -1,14 +1,16 @@
 password = 'a123456'
 n = 3
 while n > 0:
+	n = n - 1
 	p = input('請輸入密碼:')
 	if p == password:
 		print('登入成功!')
 		break
 	else:
-		n = n - 1
-		print('密碼錯誤,還有', n ,'次機會')
-		
+		print('密碼錯誤')
+		if n > 0:
+			print('還有', n ,'次機會')
+
 		
 		
 	
